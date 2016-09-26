f5121ea44f3ab252dd18aa3cc0617753a637d4f3
rkaranam <karanam.rajasekhar44@gmail.com>
2016-09-26 21:24:09 +0530

    Basic Skeleton for Spring Boot Demo Project

---
+++ src/main/java/com/boot/shipwrecks/SpringBootCourseProjectApplication.java
@@ -0,0 +1,12 @@
+package com.boot.shipwrecks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCourseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCourseProjectApplication.class, args);
+	}
+}
---
+++ src/main/java/com/boot/shipwrecks/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.boot.shipwrecks.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Das Boot, reporting for duty";
+	}
+
+}
