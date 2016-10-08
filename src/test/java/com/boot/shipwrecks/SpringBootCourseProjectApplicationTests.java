041d809b238157d05b693294da2f2bc4f50222c6
Rajasekhar Karanam <karanam.rajasekhar44@gmail.com>
2016-10-08 19:10:46 +0530

    Added a dummy junit test

--- src/test/java/com/boot/shipwrecks/SpringBootCourseProjectApplicationTests.java
+++ src/test/java/com/boot/shipwrecks/SpringBootCourseProjectApplicationTests.java
@@ -1,9 +1,13 @@
 package com.boot.shipwrecks;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.boot.shipwrecks.controller.HomeController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,13 @@ public class SpringBootCourseProjectApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void dummyTest() {
+		HomeController homeController = new HomeController();
+		String result = homeController.home();
+		assertEquals(result, "Das Boot, reporting for duty");
 	}
 
 }
