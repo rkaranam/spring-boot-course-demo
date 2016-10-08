1d922886d9b395d9df77b783bfe76a58a3693daa
Rajasekhar Karanam <karanam.rajasekhar44@gmail.com>
2016-10-08 21:10:32 +0530

    Unit Testing with Mockito

---
+++ src/test/java/com/boot/shipwrecks/controllers/ShipwreckControllerTests.java
@@ -0,0 +1,54 @@
+package com.boot.shipwrecks.controllers;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.boot.shipwrecks.controller.ShipwreckController;
+import com.boot.shipwrecks.model.Shipwreck;
+import com.boot.shipwrecks.repository.ShipwreckRepository;
+
+@SpringBootTest
+public class ShipwreckControllerTests {
+	
+	@InjectMocks
+	private ShipwreckController shipwreckController;
+	
+	@Mock
+	private ShipwreckRepository shipwreckRepository;
+	
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void getShipwreckIdTest() {
+		Shipwreck shipwreck = new Shipwreck();
+		shipwreck.setId(1L);
+		when(shipwreckRepository.findOne(1L)).thenReturn(shipwreck);
+		
+		Shipwreck wreck = shipwreckController.get(1L);
+		verify(shipwreckRepository).findOne(1L);
+		assertEquals(1L, wreck.getId().longValue());
+	}
+	
+	@Test
+	public void deleteShipwreckTest() {
+		Shipwreck shipwreck = new Shipwreck();
+		shipwreck.setId(2L);
+		when(shipwreckRepository.findOne(2L)).thenReturn(shipwreck);
+		
+		Shipwreck wreck = shipwreckController.delete(2L);
+		verify(shipwreckRepository).findOne(2L);
+		assertEquals(2L, wreck.getId().longValue());
+	}
+
+}
